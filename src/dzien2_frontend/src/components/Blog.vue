62ede5254cd5f79168935ecc761b906a1469e158
n4t4li4cz <natalkaczajko@gmail.com>
2024-06-25 19:55:23 +0200

    Dodawanie nowych wpisow i wyswietlanie ich

---
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="pobierzWpisy">refresh</button>
+        Siema Blog!
+        {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpis">dodaj</button>
+    </div>
+
+</template>
+
+<script>
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
+    export default {
+        data() {
+            return {
+                wpisy: [],
+                nowyBlog: ""
+            }
+        },
+        methods: {
+             async dodajWpis(){
+             await dzien2_backend.dodaj_wpis(this.nowyBlog);
+            },
+            async pobierzWpisy(){
+            this.wpisy =  await dzien2_backend.odczytaj_wpisy();
+            }
+        },
+    }
+</script>
