5e46e919a2bc7da43652a389d15d3a044b18562f
n4t4li4cz <natalkaczajko@gmail.com>
2024-06-26 19:08:16 +0200

    style i cos tam

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,30 +1,43 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">refresh</button>
-        Siema Blog!
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpis">dodaj</button>
-    </div>
+        <h2 class="text-color-600">Wpisy na bloga:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="float-right bg-blue-600 rounded text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4 p-4">
+        <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-100">
+            <p>{{wpis}}</p>
+        </div>
+        <div class="flex justifice-content flex-col">
+            <input v-model="nowyBlog" type="text" class="borer-2 border-blue-600 p-4">
+            <button @click="dodajWpisy" class="bg-blue-600 rounded text-white p-4">dodaj</button>
 
+        </div>
+        
+    </div>
+</div>
 </template>
 
 <script>
 import { dzien2_backend } from 'declarations/dzien2_backend/index';
-    export default {
-        data() {
-            return {
-                wpisy: [],
-                nowyBlog: ""
-            }
+
+export default {
+    data() {
+        return {
+            wpisy: [],
+            nowyBlog: ""
+        }
+    },
+    methods: {
+        async dodajWpisy() {
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
         },
-        methods: {
-             async dodajWpis(){
-             await dzien2_backend.dodaj_wpis(this.nowyBlog);
-            },
-            async pobierzWpisy(){
-            this.wpisy =  await dzien2_backend.odczytaj_wpisy();
-            }
-        },
+        async pobierzWpisy() {
+            this.wpisy = await dzien2_backend.odczytaj_wpisy();
+        }
+    },
+    async mounted(){
+        this.pobierzWpisy();
     }
+}
 </script>
